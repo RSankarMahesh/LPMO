add1fd03adb58d729ec461bf1a49d03a5771c763
R.Sankar Mahesh <shama.maha@outlook.com>
2020-11-19 23:58:27 +0530

    Plot for Eqn.4 added.

--- Equation_4_dEdT.m
+++ Equation_4_dEdT.m
@@ -1,6 +1,12 @@
 [t,Eads] = ode45(@odefun,[0 10], 1);% time value can be arbitrary
 plot(Eads,t);
+title('Equation 1');
+xlabel('Rate of adsorption');
+ylabel('Time');
 function Eads = odefun(t,Eads)
-Kxads = 8640 ;Efree = 10;f = 4;Kxdes = 19.3; % Let x be EG2; Efree and 'f' - free sites here is an arbitrary value, 
+Kxads = 8640 ;
+Efree = 10;
+f = 4;
+Kxdes = 19.3; % Let x be EG2; Efree and 'f' - free sites here is an arbitrary value, 
 dEads = Kxads*Efree*f - Kxdes*Eads;
 end
